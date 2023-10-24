1e1d2def30346589aa59b3a9d96a13e8552f6e7c
Awais Amjed <awaisamjed171@gmail.com>
2023-10-24 16:02:08 +0500

    Style progress indicator

--- src/features/onboaring/onboarding.jsx
+++ src/features/onboaring/onboarding.jsx
@@ -1,14 +1,18 @@
 import OnboardingHeader from "./header/onboarding_header.jsx";
 import {LinearProgress} from "@mui/material";
 import {useState} from "react";
+import styles from './onboarding.module.scss';
 
 const Onboarding = () => {
-  const onboardingProgress = useState(0);
+  const [onboardingProgress, setOnboardingProgress] = useState(10);
 
   return <div id={'onboarding'}>
     <OnboardingHeader />
 
-    <LinearProgress variant="determinate" value={20} />
+    <LinearProgress variant="determinate" value={onboardingProgress}
+                    className={`${styles.progressBar}`}
+    />
+
     <div>
 
     </div>
