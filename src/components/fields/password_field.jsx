1881bbfea0fa0ae4217b1fb1b05139a63cc129de
Awais Amjed <awaisamjed171@gmail.com>
2023-10-27 16:27:03 +0500

    Refactor PAssword Field

---
+++ src/components/fields/password_field.jsx
@@ -0,0 +1,44 @@
+import {IconButton, InputAdornment, TextField} from "@mui/material";
+import {Visibility, VisibilityOff} from "@mui/icons-material";
+import * as PropTypes from "prop-types";
+import {useState} from "react";
+
+const PasswordField = (props) => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  return <TextField
+    required
+    type={showPassword ? 'text' : 'password'}
+    id={props.id}
+    label={props.label}
+    value={props.value}
+    fullWidth={true}
+    error={props.showError}
+    helperText={props.showError ? 'Passwords do not match' : null}
+    onChange={props.onChange}
+    InputProps={{
+      endAdornment: <InputAdornment position="end">
+        <IconButton
+          aria-label="toggle password visibility"
+          edge="end"
+          onClick={() => {
+            setShowPassword(!showPassword);
+          }}
+        >
+          {showPassword ? <VisibilityOff /> : <Visibility />}
+        </IconButton>
+      </InputAdornment>
+    }}
+  />;
+}
+
+PasswordField.propTypes = {
+  id: PropTypes.string,
+  label: PropTypes.string,
+  value: PropTypes.string,
+  error: PropTypes.string,
+  showError: PropTypes.bool,
+  onChange: PropTypes.func,
+};
+
+export default PasswordField;
--- src/features/onboaring/slides/create_password_slide.jsx
+++ src/features/onboaring/slides/create_password_slide.jsx
@@ -1,8 +1,9 @@
 import OnboardingFooter from "../footer/onboarding_footer.jsx";
-import {Box, IconButton, InputAdornment, OutlinedInput, TextField} from "@mui/material";
+import {Box, IconButton, InputAdornment, TextField} from "@mui/material";
 import {useState} from "react";
 import {useSwiper} from "swiper/react";
 import {Visibility, VisibilityOff} from "@mui/icons-material";
+import PasswordField from "../../../components/fields/password_field.jsx";
 
 const CreatePasswordSlide = () => {
   const swiper = useSwiper();
@@ -25,56 +26,24 @@ const CreatePasswordSlide = () => {
         width={'360px'}
         className={`d-flex flex-column gap-2`}
       >
-        <TextField
-          required
+        <PasswordField
+          id={'password'}
+          label={'Password'}
           value={password}
-          type={showPassword ? 'text' : 'password'}
-          id="password"
-          label="Password"
-          fullWidth={true}
           onChange={(e) => {
             setPassword(e.target.value);
             setAllowNext(e.target.validity.valid && confirmPassword === e.target.value);
           }}
-          InputProps={{
-            endAdornment: <InputAdornment position="end">
-              <IconButton
-                aria-label="toggle password visibility"
-                edge="end"
-                onClick={() => {
-                  setShowPassword(!showPassword);
-                }}
-              >
-                {showPassword ? <VisibilityOff /> : <Visibility />}
-              </IconButton>
-            </InputAdornment>
-          }}
         />
-        <TextField
-          required
-          type={showConfirmPassword ? 'text' : 'password'}
-          id="confirmPassword"
-          label="Confirm Password"
+        <PasswordField
+          id={'confirmPassword'}
+          label={'Confirm Password'}
           value={confirmPassword}
-          fullWidth={true}
-          error={confirmPassword !== '' && password !== confirmPassword}
-          helperText={confirmPassword !== '' && password !== confirmPassword ? 'Passwords do not match' : null}
+          showError={confirmPassword !== '' && password !== confirmPassword}
+          error={'Passwords do not match'}
           onChange={(e) => {
             setConfirmPassword(e.target.value);
             setAllowNext(e.target.validity.valid && password === e.target.value);
-          }}
-          InputProps={{
-            endAdornment: <InputAdornment position="end">
-              <IconButton
-                aria-label="toggle password visibility"
-                edge="end"
-                onClick={() => {
-                  setShowConfirmPassword(!showConfirmPassword);
-                }}
-              >
-                {showConfirmPassword ? <VisibilityOff /> : <Visibility />}
-              </IconButton>
-            </InputAdornment>
           }}
         />
       </Box>
