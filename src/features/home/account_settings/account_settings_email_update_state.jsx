7f4c6a9b8bce4e5c8a1283f980374d51174c74bf
Awais Amjed <awaisamjed171@gmail.com>
2023-11-09 22:32:38 +0500

    Update email card

--- src/features/home/account_settings/account_settings_email_update_state.jsx
+++ src/features/home/account_settings/account_settings_email_update_state.jsx
@@ -1,19 +1,71 @@
-import {Typography} from "@mui/material";
+import {Box, IconButton, TextField, Typography} from "@mui/material";
 import SlideInCard from "../components/slide_in_card.jsx";
 import PropTypes from "prop-types";
+import {useState} from "react";
+import {ArrowBack} from "@mui/icons-material";
+import CustomButton from "../../../components/buttons/custom_button.jsx";
 
 const AccountSettingsEmailUpdateState = ({
   onBack,
 }) => {
+  const [email, setEmail] = useState('');
+  const [confirmEmail, setConfirmEmail] = useState('');
+
   return <SlideInCard>
     <div className={`d-flex flex-column gap-3 h-100`}
          style={{
            maxWidth: '800px',
          }}
     >
-      <Typography className={`fs-5 fw-semibold mb-3`}>
-        Your Account Settings
-      </Typography>
+      <div className={`d-flex align-items-center gap-2 mb-4`}>
+        <IconButton onClick={onBack}>
+          <ArrowBack />
+        </IconButton>
+        <Typography className={`fs-5 fw-semibold`}>
+          Update your Email
+        </Typography>
+      </div>
+
+      <Box
+        component="form"
+        width={'360px'}
+        className={`d-flex flex-column gap-3 ms-3 h-100`}
+      >
+        <Typography>
+          Add your new email
+        </Typography>
+        <TextField
+          required
+          value={email}
+          type="email"
+          id="email"
+          label="Email"
+          fullWidth={true}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        <TextField
+          required
+          value={confirmEmail}
+          type="email"
+          id="confirmEmail"
+          label="Confirm Email"
+          fullWidth={true}
+          error={confirmEmail !== '' && email !== confirmEmail}
+          helperText={confirmEmail !== '' && email !== confirmEmail ? 'Emails do not match' : null}
+          onChange={(e) => {
+            setConfirmEmail(e.target.value);
+          }}
+        />
+
+        <div className={`flex-grow-1`}/>
+
+        <CustomButton text={'Save Changes'} sx={{
+          width: '200px',
+        }}/>
+      </Box>
+
     </div>
   </SlideInCard>;
 }
