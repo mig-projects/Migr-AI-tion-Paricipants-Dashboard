431c5db94734d139c632645402b20ae6dc16653e
Awais Amjed <awaisamjed171@gmail.com>
2023-11-20 18:06:03 +0500

    Add helper functions

--- src/features/supabase/authentication.js
+++ src/features/supabase/authentication.js
@@ -24,6 +24,32 @@ const signOut = async () => {
   return { error };
 }
 
+// Get the current user's session
+const getCurrentSession = async () => {
+  const { data, error } = await supabase.auth.getSession();
+  return { data, error };
+}
+
+// Is user signed in?
+const isUserSignedIn = async () => {
+  try {
+    const {data} = await getCurrentSession();
+    return data.session !== null;
+  } catch (error) {
+    return false;
+  }
+}
+
+// Get the current user
+const getCurrentUser = async () => {
+  try {
+    const {data} = await getCurrentSession();
+    return data.session.user;
+  } catch (error) {
+    return null;
+  }
+}
+
 // Update the current user's email address
 const updateEmail = async (email) => {
   const { data, error } = await supabase.auth.updateUser({
@@ -40,4 +66,4 @@ const updatePassword = async (password) => {
   return { data, error };
 }
 
-export {signUp, signIn, signOut, updateEmail, updatePassword};
+export {signUp, signIn, signOut, updateEmail, updatePassword, getCurrentSession, isUserSignedIn, getCurrentUser};
