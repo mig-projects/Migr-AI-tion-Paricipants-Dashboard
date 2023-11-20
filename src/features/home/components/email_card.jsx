892e55c7f0edd2df7dfd19b734a282331511b2a8
Awais Amjed <awaisamjed171@gmail.com>
2023-11-20 20:56:20 +0500

    Add user email to email card

--- src/features/home/components/email_card.jsx
+++ src/features/home/components/email_card.jsx
@@ -1,6 +1,16 @@
 import {Card, Typography} from "@mui/material";
+import {useEffect, useState} from "react";
+import {getCurrentUser} from "../../supabase/authentication.js";
 
 const EmailCard = () => {
+  const [email, setEmail] = useState('');
+
+  useEffect(() => {
+    getCurrentUser().then((user) => {
+      setEmail(user.email);
+    });
+  }, []);
+
   return <Card className={`d-flex justify-content-between align-items-center`}
                elevation={0}
                style={{
@@ -9,7 +19,7 @@ const EmailCard = () => {
                }}
   >
     <Typography className={`fs-5 fw-semibold`}>FINDHR</Typography>
-    <Typography className={`fw-medium`}>name@email.com</Typography>
+    <Typography className={`fw-medium`}>{email}</Typography>
   </Card>;
 }
 
