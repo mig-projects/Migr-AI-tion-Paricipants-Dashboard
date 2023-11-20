a3e497bf7211271fa732017099a10be708b9df7e
Awais Amjed <awaisamjed171@gmail.com>
2023-11-20 20:03:32 +0500

    Update routes

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,14 @@ import {ToastContainer} from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import SignInScreen from "./features/sign_in/sign_in_screen.jsx";
 import 'swiper/scss';
+import Onboarding from "./features/onboarding/onboarding.jsx";
 
 // This component is used to render the correct page based on the URL
 const Routing = () => {
   return <Routes>
     <Route path="/sign-in" element={<SignInScreen />} />
-    <Route path="/onboarding" element={<DescribeYourExperience/>} />
+    <Route path="/onboarding" element={<Onboarding/>} />
+    <Route path="/describe-your-experience" element={<DescribeYourExperience />} />
     <Route path="/home" element={<HomeScreen />} />
     <Route path="*" element={<Navigate to='/home' />} />
   </Routes>;
