7a9b6596cf7b827352ca2ac22c76d90dc0843b36
Awais Amjed <awaisamjed171@gmail.com>
2023-11-21 17:21:05 +0500

    Add function to fetch all tags from database

---
+++ src/features/supabase/database/tags.js
@@ -0,0 +1,19 @@
+import supabase from "../configuration.js";
+
+// Fetch all tags from the database
+const fetchTags = async () => {
+  const { data: tags, error: tagsError } = await supabase.from('tags').select();
+  if (tagsError) return { data: null, tagsError: error };
+
+  const { data: tag_groups, error} = await supabase.from('tag_groups').select();
+  if (error) return { data: null, error };
+
+  const tag_groups_by_id = {};
+  tag_groups.forEach(tag_group => {
+    tag_groups_by_id[tag_group.name] = tags.filter(tag => tag.tag_group_id === tag_group.id);
+  });
+
+  return { data: tag_groups_by_id, error };
+}
+
+export { fetchTags } from "./tags.js";
