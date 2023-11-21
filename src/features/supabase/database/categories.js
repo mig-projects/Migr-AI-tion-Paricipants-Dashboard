68259ac46157a202d6349646aa5aec1860ff47e7
Awais Amjed <awaisamjed171@gmail.com>
2023-11-21 17:12:31 +0500

    Add function to get all categories

---
+++ src/features/supabase/database/categories.js
@@ -0,0 +1,9 @@
+import supabase from "../configuration.js";
+
+// Fetch all categories from the database
+const fetchCategories = async () => {
+  const { data, error } = await supabase.from('categories').select('name');
+  return { data, error };
+}
+
+export { fetchCategories } from "./categories.js";
