74574519b2ac447aeb488ca2a3d57a8f89ae734b
Awais Amjed <awaisamjed171@gmail.com>
2023-11-23 20:08:12 +0500

    Try another way of using env variables

--- src/features/supabase/configuration.js
+++ src/features/supabase/configuration.js
@@ -1,9 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
+
 // Create a single supabase client for interacting with your database
-const supabase = createClient(
-  process.env.VITE_SUPABASE_URL,
-  process.env.VITE_SUPABASE_ANON_KEY
-);
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export default supabase;
