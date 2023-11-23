d35b18b674124215e4947c965e4e69dd6586714b
Awais Amjed <awaisamjed171@gmail.com>
2023-11-23 11:48:37 +0500

    Searchable iteration of select

--- src/features/describe_your_experience/components/custom_select.jsx
+++ src/features/describe_your_experience/components/custom_select.jsx
@@ -1,4 +1,4 @@
-import {Checkbox, FormControl, ListItemText, MenuItem, Select} from "@mui/material";
+import {Autocomplete, Checkbox, FormControl, ListItemText, MenuItem, TextField} from "@mui/material";
 import PropTypes from "prop-types";
 
 const CustomSelect = ({
@@ -8,34 +8,26 @@ const CustomSelect = ({
   list,
 }) => {
   return <FormControl variant={`outlined`} sx={{ m: 1, minWidth: 150 }}>
-    <Select
+    <Autocomplete
       sx={{
         '.MuiOutlinedInput-notchedOutline': { border: 0 },
-        maxWidth: '300px',
+        minWidth: '150px',
+        width: '100%',
       }}
       multiple
-      value={value}
-      onChange={(event) => {
-        onChange(event.target.value);
-      }}
-      displayEmpty
-      inputProps={{ outline: "none" }}
-      renderValue={(selected) => selected.length > 0 ? selected.map((e) => e.name).join(', ') : title}
-      MenuProps={{
-        PaperProps: {
-          elevation: 0,
-        },
-      }}
-    >
-      {
-        list.map((tag) => (
-          <MenuItem key={tag.id} value={tag}>
-            <Checkbox checked={value.indexOf(tag) > -1} />
-            <ListItemText primary={tag.name} />
-          </MenuItem>
-        ))
-      }
-    </Select>
+      disableCloseOnSelect
+      getOptionLabel={(option) => option.name}
+      renderInput={(params) => (
+        <TextField {...params} label={title} />
+      )}
+      renderOption={(props, option, { selected }) => (
+        <MenuItem key={option.id} value={option} {...props}>
+          <Checkbox checked={selected} />
+          <ListItemText primary={option.name} />
+        </MenuItem>
+      )}
+      options={list}
+    />
   </FormControl>;
 }
 
