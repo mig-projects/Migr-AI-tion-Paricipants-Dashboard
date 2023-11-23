fddf8b541dfb107a0a00b5ae1906ce67c0e304bf
Awais Amjed <awaisamjed171@gmail.com>
2023-11-23 19:44:33 +0500

    Update secrets import

--- src/features/supabase/configuration.js
+++ src/features/supabase/configuration.js
@@ -2,8 +2,8 @@ import { createClient } from '@supabase/supabase-js';
 
 // Create a single supabase client for interacting with your database
 const supabase = createClient(
-  process.env.REACT_APP_SUPABASE_URL,
-  process.env.REACT_APP_SUPABASE_ANON_KEY
+  import.meta.env.REACT_APP_SUPABASE_URL,
+  import.meta.env.REACT_APP_SUPABASE_ANON_KEY
 );
 
 export default supabase;
