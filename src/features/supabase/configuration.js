efdbe738e222a12b71340e3c212667413f544882
Awais Amjed <awaisamjed171@gmail.com>
2023-11-23 19:55:25 +0500

    Rename env variables

--- src/features/supabase/configuration.js
+++ src/features/supabase/configuration.js
@@ -2,8 +2,8 @@ import { createClient } from '@supabase/supabase-js';
 
 // Create a single supabase client for interacting with your database
 const supabase = createClient(
-  process.env.REACT_APP_SUPABASE_URL,
-  process.env.REACT_APP_SUPABASE_ANON_KEY
+  process.env.VITE_SUPABASE_URL,
+  process.env.VITE_SUPABASE_ANON_KEY
 );
 
 export default supabase;
