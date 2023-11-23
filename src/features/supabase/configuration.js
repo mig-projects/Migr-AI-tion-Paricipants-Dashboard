3277e529368eb72f0e9f2f04d5d48c7164b4823f
Awais Amjed <awaisamjed171@gmail.com>
2023-11-23 19:37:45 +0500

    Update supabase config for production

--- src/features/supabase/configuration.js
+++ src/features/supabase/configuration.js
@@ -1,7 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
-import config from './config.json';
 
 // Create a single supabase client for interacting with your database
-const supabase = createClient(config.url, config.key);
+const supabase = createClient(
+  process.env.REACT_APP_SUPABASE_URL,
+  process.env.REACT_APP_SUPABASE_ANON_KEY
+);
 
 export default supabase;
