956ae51055fe1d1041c45d1745762237db84208a
Awais Amjed <awaisamjed171@gmail.com>
2023-11-23 20:08:58 +0500

    Remove as string

--- src/features/supabase/configuration.js
+++ src/features/supabase/configuration.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 // Create a single supabase client for interacting with your database
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
